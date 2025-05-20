38f3f76a7739521f9ae6c29fe1618b74b90ba4bf
Mobin Haque <101964870+MobinTGR8@users.noreply.github.com>
2025-05-21 01:06:37 +0600

    Delete BagStore_Ecommerce directory

--- BagStore_Ecommerce/includes/footer.php
+++
@@ -1,7 +0,0 @@
-<footer style="background:#333; color:#fff; text-align:center; padding:15px; margin-top:40px;">
-    <p>&copy; <?= date('Y') ?> BagStore. All rights reserved.</p>
-    <p>
-        <a href="index.php" style="color:#0af; text-decoration:none;">Home</a> | 
-        <a href="contact.php" style="color:#0af; text-decoration:none;">Contact Us</a>
-    </p>
-</footer>
